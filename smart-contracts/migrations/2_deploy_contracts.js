0376b0ab0785ffa70aabac9757e9cba62351b398
Nick Furfaro <nfurfaro33@gmail.com>
2018-12-29 22:42:39 -0700

    Implement deploying Unlock proxy using zos in dev

--- smart-contracts/migrations/2_deploy_contracts.js
+++ smart-contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,21 @@
 const Unlock = artifacts.require('./Unlock.sol')
+const shell = require('shelljs')
 
-module.exports = function deployContracts (deployer, network, accounts) {
-  return deployer.deploy(Unlock, accounts[0])
+module.exports = function (deployer, network, accounts) {
+  deployer.then(() => {
+    if (network !== 'test') {
+      if (
+        shell.exec(
+          `zos create Unlock --init initialize --args ${
+            accounts[0]
+          } --network ${network} --from ${accounts[9]}`
+        ).code !== 0
+      ) {
+        throw new Error('Migration failed')
+      }
+    } else {
+      console.log("Skipping proxy creation in 2_deploy_contracts.js while on network 'test'")
+      return deployer.deploy(Unlock, accounts[0])
+    }
+  })
 }
